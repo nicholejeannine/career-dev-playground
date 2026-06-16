41e794d7de68776d77eb947d52c9fda69278f3cb
nicholejeannine <nicholejeannine@protonmail.com>
2026-06-15 21:42:13 -0700

    Adds python project and some simple starter comments, data types etc. much more todo

---
+++ python_project/simple.py
@@ -0,0 +1,22 @@
+# Python comments
+'''Multiline comments
+can be written thusly'''
+
+# assign multiple vars at once
+a, b, c = 5, 3.2, 'Hello'
+
+# define complex numbers, e.g., with imaginary number `i` (designated as `j` in Python)
+
+a = 9 + 3j
+b = 3 + 4j
+print(a + b) # prints (12 + 7j)
+
+# Boolean literal are written with Titlecase
+is_pass = True
+print(is_pass) # Prints True
+
+# Null is a special literal 'None'
+value = None
+
+print(value) # Output: None
+
